3210d1f1fae7045c0e11781d3fccb607fd16a495
Maravello <yahayac753@gmail.com>
2026-04-06 14:42:10 +0200

    commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import { PrismaClient } from "@prisma/client/edge";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-let prisma: PrismaClient
+let prisma: PrismaClient;
 
 if (!globalForPrisma.prisma) {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-  const adapter = new PrismaPg(pool)
-  prisma = new PrismaClient({ adapter })
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  prisma = new PrismaClient({ adapter });
 } else {
-  prisma = globalForPrisma.prisma
+  prisma = globalForPrisma.prisma;
 }
 
-export { prisma }
+export { prisma };
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
